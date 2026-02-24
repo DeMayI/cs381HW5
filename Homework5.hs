e6586f5567d38d7007d1fdb647eb96d600a7454f
demayi <demayi@oregonstate.edu>
2026-02-24 13:24:55 -0800

    Started work on rankP

--- Homework5.hs
+++ Homework5.hs
@@ -12,3 +12,27 @@ rankC (POP k) = (k,0)
 rankC (IFELSE p1 p2) = (1,0)
 rankC (LDB b) = (0,1)
 rankC LEQ = (2,1)
+
+rankB :: CmdRank -> Rank -> Bool
+rankB (p, i) r = ((r - p) >= 0)
+
+nRank :: CmdRank -> Rank -> Rank
+nRank (p, i) r = r - p + i
+
+minRank ::  Maybe Rank -> Maybe Rank -> Maybe Rank
+minRank Nothing Nothing = Nothing
+minRank Nothing _ = Nothing
+minRank _ Nothing = Nothing
+minRank x y 
+    | x < y = x
+    | otherwise = y
+
+
+rankP :: Prog -> Rank -> Maybe Rank
+rankP [] r = Just r
+rankP ((IFELSE p1 p2):cs) r 
+    | rankB (rankC (IFELSE p1 p2)) r && minRank (rankP p1 (nRank (rankC (IFELSE p1 p2)) r)) (rankP p2 (nRank (rankC (IFELSE p1 p2)) r)) /= Nothing = rankP cs (minRank (rankP p1 (nRank (rankC (IFELSE p1 p2)) r)) (rankP p2 (nRank (rankC (IFELSE p1 p2)) r)))
+    | otherwise = Nothing
+rankP (c:cs) r 
+    | rankB (rankC c) r = rankP cs (nRank (rankC c) r)
+    | otherwise = Nothing
