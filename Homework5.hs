1d1ed3fa1c38535100a7c1b98f92464e1ad84e0b
demayi <demayi@oregonstate.edu>
2026-02-24 19:51:15 -0800

    Added the very necessary comments

--- Homework5.hs
+++ Homework5.hs
@@ -20,12 +20,15 @@ rankC LEQ = (2,1)
 rankC INC = (1,1)
 rankC _ = (0,0)
 
+--Returns true if a cmd is runnable and wouldn't cause the rank to drop below zero
 rankB :: CmdRank -> Rank -> Bool
 rankB (p, i) r = ((r - p) >= 0)
 
+--Returns the new rank of the stack after the commmand has been applied
 nRank :: CmdRank -> Rank -> Rank
 nRank (p, i) r = r - p + i
 
+--Returns the smallest rank, or nothing if either argument is nothing
 minRank ::  Maybe Rank -> Maybe Rank -> Maybe Rank
 minRank Nothing Nothing = Nothing
 minRank Nothing _ = Nothing
@@ -36,8 +39,14 @@ minRank x y
 
 
 rankP :: Prog -> Rank -> Maybe Rank
+
+--If the program is empty return the current rank
 rankP [] r = Just r
 
+--Handles the ifelse statement ranks,
+--First checks to see ifelse statement itself is rank Safe
+--Then checks each child progrm to make sure they are rank safe, returning nothing if one isn't rank safe
+--And returning the minimum rank if both are rank safe
 rankP ((IFELSE p1 p2):cs) r
   | rankB (rankC (IFELSE p1 p2)) r
     , let mr = minRank (rankP p1 r1) (rankP p2 r1)
@@ -48,21 +57,30 @@ rankP ((IFELSE p1 p2):cs) r
       Nothing -> Nothing         
 
   | otherwise = Nothing
+
+--Default case, Checks to see if the cmd is rank safe, if so calls rankp on the next command with the new rank
+--If the cmd is not rank safe, return nothing
 rankP (c:cs) r 
     | rankB (rankC c) r = rankP cs (nRank (rankC c) r)
     | otherwise = Nothing
 
+--Calculates the rank of a stack
 stackToRank :: Stack -> Rank
 stackToRank [] = 0
 stackToRank (x:xs) = 1 + stackToRank xs
 
 
+--Wrapper function for sem,
+--Calls rankP to ensure that the prog is rank safe
+--if so it returns sem, otherwise it returns a rank error
 run :: Prog -> Stack -> Result
 run p s
   = case rankP p (stackToRank s) of
     Nothing -> RankError
     Just x -> sem p s
 
+--Iterates through the program until it reaches an empty list, at which point it will return an empty stack
+--If any typeerrors are detected, return a TypeError instead
 sem :: Prog -> Stack -> Result
 sem [] s = A s       -- no program: succeed with current stack
 sem (c:cs) s =
